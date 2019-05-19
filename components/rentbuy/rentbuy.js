d3afed0c594ad99a3ae2071c64ebe1f5db37edd7
Junior Dugue <jrdugue@gmail.com>
2019-05-18 21:00:05 -0400

    added image hover effect and js for enlarging image and closing the enlarge img display

--- components/rentbuy/rentbuy.js
+++ components/rentbuy/rentbuy.js
@@ -1,4 +1,4 @@
-// typewriter effect for the h3 tag
+// typewriter effect for the h3 and h4 tag
 let i=0,h3Text;
 let j=0,h4Text;
 
@@ -7,7 +7,7 @@ h4Text = "Explore Our Math TextBooks"
 
 function typingH3(){
   if(i<h3Text.length){
-    document.getElementById("h3Text").innerHTML += h3Text.charAt(i);
+    document.getElementById("h3Text").textContent += h3Text.charAt(i);
     i++;
     setTimeout(typingH3,50);
   }
@@ -15,11 +15,48 @@ function typingH3(){
 
 function typingH4(){
   if(j<h4Text.length){
-    document.getElementById("h4Text").innerHTML += h4Text.charAt(j);
+    document.getElementById("h4Text").textContent += h4Text.charAt(j);
     j++;
     setTimeout(typingH4,50);
   }
 }
 
-typingH3();
+
+////////  mathbooks image gallery  ////////
+
+const onMathImageClick = () => {
+  // selecting all of the math books images
+  const mathImagesList = document.querySelectorAll(".math_img");
+  // to turn my list of images into an array i used the spread operator 
+  const mathImage = [...mathImagesList];
+// looping through the math images
+  mathImage.forEach(image => {
+    // event listener for the click 
+    image.addEventListener("click", event => {
+      mathImageOpen(event.target);
+    })
+  })
+}
+// for the expanded image
+const mathImageOpen = image => {
+  const imageSrc = image.getAttribute("src");
+  // this is for the enlarged image when the small image is clicked
+  const openedImage = `<div class="math-backdrop"><img src="${imageSrc}" alt=""/>
+                       <span class="math-backdrop-close">x</span></div>`;
+  // specifying where i want my image to appear on the web page
+  document.body.insertAdjacentHTML("beforeend", openedImage);
+  mathImageClose();
+}
+
+// for the x button to close image
+  const mathImageClose = () => {
+    const closeButton = document.querySelector(".math-backdrop-close");
+    closeButton.addEventListener("click", () => {
+      const backdrop = document.querySelector(".math-backdrop");
+      backdrop.remove();
+    })
+  }
+
+typingH3(); 
 typingH4();
+onMathImageClick();
