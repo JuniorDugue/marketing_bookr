617e36ab2839ab1b605ca775e1ff9b74c9bde1a8
Junior Dugue <jrdugue@gmail.com>
2019-05-22 22:55:18 -0400

    added js to the stars on the index page, edited the less css file to reflect the dynamic interaction when rating a book, started working on a pop modal for when users add books to their quueue, also added footer to the pages and fixed the nav via rentbuy page

--- components/testimonials/testimonials.js
+++ components/testimonials/testimonials.js
@@ -1,2 +1,33 @@
-const selector = document.querySelector('.yourdiv')
-selector.classList.add('magictime', 'puffIn')
+//initial setup
+document.addEventListener("DOMContentLoaded", function(){
+  let stars = document.querySelectorAll(".star");
+  stars.forEach(function(star){
+    star.addEventListener("click", setRating);
+  });
+
+//make sure we're working with a number and not a string, turning it in a integer
+  let rating = parseInt(document.querySelector(".stars").getAttribute("data-rating"));
+  let target = stars[rating - 1];
+  target.dispatchEvent(new MouseEvent("click"));
+});
+
+//
+function setRating(ev){
+  let span = ev.currentTarget;
+  let stars = document.querySelectorAll(".star");
+  let match = false;
+  let num = 0;
+  stars.forEach(function(star){
+    if(match){
+      star.classList.remove("rated");
+    } else {
+      star.classList.add("rated");
+    }
+    if(star === span){
+      match = true;
+      num = index + 1;
+    }
+    let starValue = parseInt(star.getAttribute("data-val"));
+  })
+  document.querySelector(".stars").setAttribute("data-rating", num);
+}
